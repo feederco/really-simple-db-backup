2c92398800fef57d4848e641e135f3de90782965
Erik Rothoff Andersson <erik.rothoff@gmail.com>
2020-03-09 08:52:56 +0100

    Implement rretry in GetRunningInstance data which calls a magic IP in the DO datacenter. It has a habit of timing out

--- pkg/running_instance_helper.go
+++ pkg/running_instance_helper.go
@@ -1,11 +1,22 @@
 package pkg
 
 import (
+	"time"
+
 	"github.com/digitalocean/go-metadata"
 )
 
 // GetRunningInstanceData returns current droplets data
 func GetRunningInstanceData() (*metadata.Metadata, error) {
-	client := metadata.NewClient()
-	return client.Metadata()
+	var err error
+	var result *metadata.Metadata
+	for tries := 0; tries < 5; tries++ {
+		client := metadata.NewClient()
+		result, err = client.Metadata()
+		if err == nil {
+			return result, nil
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
+	return nil, err
 }
